b84ba294550c8d7a4aba48a264a93ad1977c4297
kshitij619 <vyaskshitij000@gmail.com>
2024-07-31 20:32:17 +0530

    Before showcase

--- Project14/pokemon.js
+++ Project14/pokemon.js
@@ -1,16 +1,22 @@
-let pokeApi = fetch("https://pokeapi.co/api/v2/pokemon/?limit=100");
+let pokeApi = fetch("https://pokeapi.co/api/v2/pokemon/?limit=9");
 
 async function getRes() {
   let result = await pokeApi;
   let all = await result.json();
   for (let pokemon of all.results) {
+    // async function information (){
+    //   let info;
+    // }
+
     let info = fetch(pokemon.url)
-      .then(function (getIt) {
-        return getIt.json();
+      .then(function (pokeInfoAll) {
+        return pokeInfoAll.json();
       })
-      .then(function (getIt) {
+      .then(function (pokeInfoAll) {
         console.log(pokemon.name);
-        console.log(getIt);
+        console.log(pokeInfoAll);
+        console.log("Abilities: ");
+        console.log(pokeInfoAll.abilities);
       });
   }
 }
